6a8eb1c7e6b507bfba68ba48e8c147b3583dc2c8
dybik08 <dybik08@gmail.com>
2018-07-16 20:27:48 +0200

    Add Nasa API to app

--- src/router.js
+++ src/router.js
@@ -1,21 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Search from './views/Search.vue';
 import About from './views/About.vue';
 
 Vue.use(Router);
 
 export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'Home',
-            component: Home,
-        },
-        {
-            path: '/about',
-            name: 'About',
-            component: About,
-        },
-    ],
+  routes: [
+    {
+      path: '/',
+      name: 'Search',
+      component: Search,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About,
+    },
+  ],
 });
